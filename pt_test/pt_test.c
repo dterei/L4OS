ff520890158f1664e56a0388346a70ec1985bec6
David Terei <david@david-laptop.(none)>
2008-08-26 17:35:42 +1000

    Added pt_test to user programs.

---
+++ pt_test/pt_test.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <assert.h>
+
+// TODO: xxx HACK needed for printf. Need to move printf to libc
+#include "../tty_test/ttyout.c"
+
+#define NPAGES 128
+
+/* called from pt_test */
+static void
+do_pt_test( char *buf )
+{
+	int i;
+	//L4_Fpage_t fp;
+
+	/* set */
+	for(i = 0; i < NPAGES; i += 4)
+		buf[i * 1024] = i;
+
+	/* flush */
+	assert(!"make a syscall to flush the user address space");
+	//sos_debug_flush();
+
+	/* check */
+	for(i = 0; i < NPAGES; i += 4)
+		assert(buf[i*1024] == i);
+}
+
+static void
+pt_test( void )
+{
+	/* need a decent sized stack */
+	char buf1[NPAGES * 1024], *buf2 = NULL;
+
+	/* check the stack is above phys mem */
+	assert((void *) buf1 > (void *) 0x2000000);
+
+	/* stack test */
+	do_pt_test(buf1);
+
+	/* heap test */
+	buf2 = malloc(NPAGES * 1024);
+	assert(buf2);
+	do_pt_test(buf2);
+	free(buf2);
+}
+
+int
+main( int argc, char *argv[])
+{
+	pt_test();
+}
+
