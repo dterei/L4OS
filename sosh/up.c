50e6d496545842897766f2c3d58bcec140b5c37a
David Anthony Terei <davidterei@gmail.com>
2008-10-08 01:14:00 +1100

    Added hours and days to up.c

--- sosh/up.c
+++ sosh/up.c
@@ -7,13 +7,20 @@ int up(int argc, char **argv)
 {
 	uint64_t us = uptime();
 	uint64_t us2 = us;
-	uint64_t secs = us / 1000000;
-	uint64_t mins = secs / 60;
 
+	uint64_t secs = us / 1000000;
 	us2 -= 1000 * secs;
+
+	uint64_t mins = secs / 60;
 	secs -= 60 * mins;
 
-	printf("up %llu mins %llu secs %llu us (total %llu)\n", mins, secs, us2, us);
+	uint64_t hours = mins / 60;
+	mins -= 60 * hours;
+
+	uint64_t days = hours / 24;
+	hours -= 24 * days;
+
+	printf("up %llu days %llu hours %llu mins %llu secs %llu us (total %llu)\n", days, hours, mins, secs, us2, us);
 	return 0;
 }
 
