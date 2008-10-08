36649dd021c4d0ab2ee222a8de732d07f69d8617
Ben Kalman <ben@voyager.(none)>
2008-10-08 12:34:54 +1100

    Enable bootinfo programs

--- bin/hello3/hello3.c
+++ bin/hello3/hello3.c
@@ -2,13 +2,18 @@
 #include <sos/sos.h>
 
 #define SLEEP_MS 3000
+#define SIZE 1024
 
 int main(int argc, char *argv[]) {
 	char buf[64];
+	int *ptr;
 
 	for (int i = 0;; i++) {
 		sprintf(buf, ">>> hello from %d, iteration %d\n", my_id(), i);
 		kprint(buf);
+		ptr = (int*) malloc(SIZE);
+		*ptr = i;
+		assert(*ptr == i);
 		usleep(SLEEP_MS * 1000);
 	}
 
