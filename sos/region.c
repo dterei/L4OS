301660044c9cffc1c162a50940a83dd2c349d49c
Ben Kalman <ben@phaser.(none)>
2008-10-11 12:28:10 +1100

    Add missing region.c/h files

---
+++ sos/region.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+
+#include "region.h"
+
+struct Region_t {
+	region_type type;
+	uintptr_t base;
+	unsigned int size;
+	int rights;
+	int mapDirectly;
+	struct Region_t *next;
+};
+
+Region *region_alloc(region_type type, uintptr_t base,
+		uintptr_t size, int rights, int dirmap) {
+	Region *new = (Region*) malloc(sizeof(Region));
+
+	new->type = type;
+	new->base = base;
+	new->size = size;
+	new->rights = rights;
+	new->mapDirectly = dirmap;
+
+	return new;
+}
+
+void region_free(Region *r) {
+	free(r);
+}
+
+region_type region_get_type(Region *r) {
+	return r->type;
+}
+
+uintptr_t region_get_base(Region *r) {
+	return r->base;
+}
+
+unsigned int region_get_size(Region *r) {
+	return r->size;
+}
+
+int region_get_rights(Region *r) {
+	return r->rights;
+}
+
+int region_map_directly(Region *r) {
+	return r->mapDirectly;
+}
+
+void region_set_rights(Region *r, int rights) {
+	r->rights = rights;
+}
+
+void region_set_size(Region *r, unsigned int size) {
+	r->size = size;
+}
+
---
+++ sos/region.h
@@ -0,0 +1,35 @@
+#ifndef _SOS_REGION_H_
+#define _SOS_REGION_H_
+
+#define REGION_READ 0x4
+#define REGION_WRITE 0x2
+#define REGION_EXECUTE 0x1
+
+typedef enum {
+	REGION_STACK,
+	REGION_HEAP,
+	REGION_OTHER,
+	REGION_THREAD_INIT
+} region_type;
+
+typedef struct Region_t Region;
+
+// Allocate new region with given properties
+Region *region_alloc(region_type type, uintptr_t base,
+		uintptr_t size, int rights, int dirmap);
+
+// Free region
+void region_free(Region *r);
+
+// Getters
+region_type region_get_type(Region *r);
+uintptr_t region_get_base(Region *r);
+unsigned int region_get_size(Region *r);
+int region_get_rights(Region *r);
+int region_map_directly(Region *r);
+
+// Setters
+void region_set_rights(Region *r, int rights);
+void region_set_size(Region *r, unsigned int size);
+
+#endif // sos/region.h
