cf163aabd7eef861abce23060dc6ce796f47b1e2
Benjamin Kalman <bkalman@cse.unsw.EDU.AU>
2008-10-17 13:58:21 +1100

    Add ls back to sosh

--- bin/sosh/sosh.c
+++ bin/sosh/sosh.c
@@ -74,6 +74,114 @@ struct command {
 	int (*command)(int argc, char **argv);
 };
 
+struct arg {
+	char *arg;
+	int set;
+};
+
+#define LINE_LEN 80
+#define ARG_COUNT 4
+
+#define ARG_A 0
+#define ARG_L 1
+
+static
+struct arg args[] = {
+	{"a", 0},
+	{"l", 0},
+};
+
+static void prstat(const char *name) {
+	// array corresponding to file types.
+	static char type[] = {'s', '-', 'd'};
+	// make sure we don't go out of array
+	int t = sbuf.st_type > 2 ? 0 : sbuf.st_type;
+
+	printf("%c%c%c%c %08u 0x%08lx 0x%08lx %s\n",
+			type[t],
+			sbuf.st_fmode & FM_READ     ? 'r' : '-',
+			sbuf.st_fmode & FM_WRITE    ? 'w' : '-',
+			sbuf.st_fmode & FM_EXEC     ? 'x' : '-',
+			sbuf.st_size, sbuf.st_ctime, sbuf.st_atime, name);
+}
+
+static int ls(int argc, char **argv) {
+	int i, r;
+	char buf[IO_MAX_BUFFER];
+
+	if (argc > ARG_COUNT) {
+		printf("usage: %s [-a] [-l] [file]\n", argv[0]);
+		return 1;
+	}
+
+	//reset the args
+	for (int j = 0; j < sizeof(args) / sizeof(struct arg); j++) {
+		args[j].set = 0;
+	}
+
+	// Pass args
+	for (int j = 1; j < argc; j++) {
+		if (strncmp(argv[j], "-", 1) == 0) {
+			for (int j2 = 1; j2 < strlen(argv[j]); j2++) {
+				for (int j3 = 0; j3 < sizeof(args) / sizeof(struct arg); j3++) {
+					if (strncmp(&argv[j][j2], args[j3].arg, 1) == 0) {
+						args[j3].set = 1;
+					}
+				}
+			}
+		}
+	}
+
+	if (argc > 1 && (strncmp(argv[argc - 1], "-", 1) != 0)) {
+		r = stat(argv[argc - 1], &sbuf);
+
+		if (r < 0) {
+			printf("stat(%s) failed: %s\n", argv[argc - 1], sos_error_msg(r));
+			return 0;
+		}
+
+		prstat(argv[argc - 1]);
+		return 0;
+	}
+
+	int linec = 0;
+	for (i = 0;; i++) {
+		r = getdirent(i, buf, IO_MAX_BUFFER);
+
+		if (r == SOS_VFS_EOF) {
+			break;
+		} else if (r < 0) {
+			printf("dirent(%d) failed: %s\n", i, sos_error_msg(r));
+			break;
+		}
+
+		if (args[ARG_A].set == 1 || strncmp(buf, ".", 1) != 0) {
+			if (args[ARG_L].set == 1) {
+				r = stat(buf, &sbuf);
+				if (r < 0) {
+					printf("stat(%s) failed: %s\n", buf, sos_error_msg(r));
+					break;
+				}
+
+				prstat(buf);
+			} else {
+				if (linec + strlen(buf) > LINE_LEN) {
+					printf("\n");
+					linec = 0;
+				}
+				linec += printf("%s ", buf);
+			}
+		} else {
+			continue;
+		}
+	}
+
+	if (args[ARG_L].set != 1) {
+		printf("\n");
+	}
+
+	return 0;
+}
 static int kill(int argc, char **argv) {
 	if (argc < 2) {
 		printf("usage: %s pid\n", argv[0]);
@@ -158,6 +266,7 @@ struct command sosh_commands[] = {
 	{"exec", exec},
 	{"help", help},
 	{"kill", kill},
+	{"ls", ls},
 	{"pid", pid},
 	{"segfault", segfault},
 	//{"time", time},
