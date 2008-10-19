0171639257dd2984614a7a6a1fc795229df444bc
David Anthony Terei <davidterei@gmail.com>
2008-10-20 02:40:50 +1100

    Add missing timer.h. Am trying to remove some of the stuff out of libsos since libsos is quite horrible.

---
+++ sos/timer.h
@@ -0,0 +1,15 @@
+#ifndef _TIMER__H_
+#define _TIMER__H_
+
+// Init the timer
+void utimer_init(void);
+
+// sos_usleep(uint32_t microseconds)
+//
+// Put the calling thread to sleep for microseconds.  This function is
+// implemented on a temporary L4 debugger api and the timer.c 'timer' driver.
+// You will need to reimplment this function when you finish your clock driver.
+//
+void utimer_sleep(uint32_t microseconds);
+
+#endif
